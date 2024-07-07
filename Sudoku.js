eb33866e1deec5835adf21d436af2b72afa5fbca
ShockedWhistle <78836929+ShockedWhistle@users.noreply.github.com>
2024-07-07 06:27:06 -0600

    Decided to do this in an object oriented way

--- Sudoku.js
+++ Sudoku.js
@@ -1,7 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import {useState, useEffect} from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
 import { getSudoku } from 'sudoku-gen';
 
 export default function Sudoku({ navigation, route}) {
@@ -10,20 +9,34 @@ export default function Sudoku({ navigation, route}) {
     console.log("Sudoku: ", route.params.difficulty.value);
     useEffect(() => {
         navigation.setOptions({title: "Sudoku "+ route.params.difficulty.label});
+        console.log(sudoku);
+        if(sudoku == null){
+            if(route.params.sudoku == null){ // Make new board
+                setSudoku(getSudoku(route.params.difficulty.value));
+            }
+            else{ // Remake board
+
+            }
+        }
     });
+
+    function displaySudoku(){
+        return(
+            <Text>Display Sudoku board object that has a cell object that has individual box objects</Text>
+        );
+    }
 
     const styles = StyleSheet.create({
         container: {
             flex: 1,
             backgroundColor: '#fff',
-            alignItems: 'center',
             justifyContent: 'center',
         },
     });
 
     return (
         <View style={styles.container}>
-            <Text>Open up App.js to start working on your app</Text>
+            {sudoku !== null && displaySudoku()}
             <StatusBar style="auto" />
         </View>
     );
---
+++ classess/board.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { getSudoku } from 'sudoku-gen';
+
+
+export default class Board{
+    constructor(difficulty){
+        
+    }
+    setBoard(board){
+
+    }
+}
