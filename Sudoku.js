c778e25f811729673f5338e8b91a5d347a898eec
ShockedWhistle <78836929+ShockedWhistle@users.noreply.github.com>
2024-09-08 03:58:28 -0600

    Update Sudoku.js

--- Sudoku.js
+++ Sudoku.js
@@ -8,6 +8,7 @@ import { getSudoku } from 'sudoku-gen';
 export default function Sudoku({ navigation, route}) {
     const [sudoku, setSudoku] = useState(null);
     const [focus, setFocus] = useState(null);
+    const [highlighted, setHighlighted] = useState(null);
     const [isTemp, setIsTemp] = useState(false);
     const [isLock, setIsLock] = useState(false);
     const [refresh, setRefresh] = useState('');
@@ -26,7 +27,7 @@ export default function Sudoku({ navigation, route}) {
                 puzzleStr: sudokuStr.puzzle,
                 solutionStr: sudokuStr.solution,
                 difficulty: sudokuStr.difficulty,
-                cells: [[], [], [], [], [], [], [], [], []],
+                boxes: [],
                 mistakes: 0,
             };
             let currentCell = 0;
@@ -48,7 +49,7 @@ export default function Sudoku({ navigation, route}) {
                     }
                 }
 
-                let tempBox = {id: tempSudoku.cells[currentCell].length, cellId: currentCell, current: currentPuzzle, solution: currentSolution, temp: []};
+                let tempBox = {id: tempSudoku.boxes.length, cellId: currentCell, current: currentPuzzle, solution: currentSolution, temp: []};
                 if(tempBox.cellId == 0 || tempBox.cellId == 3|| tempBox.cellId == 6){
                     if(tempBox.id == 0 || tempBox.id == 3 || tempBox.id == 6){
                         tempBox.colum = 0;
@@ -117,7 +118,7 @@ export default function Sudoku({ navigation, route}) {
                     }
                 }
 
-                tempSudoku.cells[currentCell].push(tempBox);
+                tempSudoku.boxes.push(tempBox);
             }
             saveSudoku(tempSudoku);
         }
@@ -130,28 +131,30 @@ export default function Sudoku({ navigation, route}) {
         AsyncStorage.setItem('sudoku', JSON.stringify(tempSudoku));
     }
 
-    function changeFocus(cellId, boxId){
+    function changeFocus(boxId){
         setRefresh('');
-        setFocus([cellId, boxId]);
-        console.log(sudoku.cells[cellId][boxId]);
+        setFocus(boxId);
+        
     }
     function placeNumber(number){
-        const box = sudoku.cells[focus[0]][focus[1]];
+        const box = sudoku.boxes.find((e) => e.id == focus);
         if(box!== null && box.current != box.solution){
             let tempSudoku = sudoku;
+            const boxIndex = tempSudoku.boxes.findIndex((e) => e.id == focus);
+
             if(isTemp){
-                const tempIndex = tempSudoku.cells[focus[0]][focus[1]].temp.findIndex((e) => e == number);
+                const tempIndex = tempSudoku.boxes[boxIndex].temp.findIndex((e) => e == number);
                 if(tempIndex == -1){
-                    tempSudoku.cells[focus[0]][focus[1]].temp.push(number);
+                    tempSudoku.boxes[boxIndex].temp.push(number);
                 }
                 else{
-                    tempSudoku.cells[focus[0]][focus[1]].temp.splice(tempIndex, 1);
+                    tempSudoku.boxes[boxIndex].temp.splice(tempIndex, 1);
                 }
                 saveSudoku(tempSudoku);
             }
             else{
-                tempSudoku.cells[focus[0]][focus[1]].current = number;
-                if(number != tempSudoku.cells[focus[0]][focus[1]].solution){
+                tempSudoku.boxes[boxIndex].current = number;
+                if(number != tempSudoku.boxes[boxIndex].solution){
                     tempSudoku.mistakes++;
                 }
                 saveSudoku(tempSudoku);
@@ -164,19 +167,19 @@ export default function Sudoku({ navigation, route}) {
         return(
             <View style={styles.board}>
                 <View style={styles.row}>
-                    {displayCell(sudoku.cells[0])}
-                    {displayCell(sudoku.cells[1])}
-                    {displayCell(sudoku.cells[2])}
+                    {displayCell(sudoku.boxes.filter((e) => e.cellId == 0))}
+                    {displayCell(sudoku.boxes.filter((e) => e.cellId == 1))}
+                    {displayCell(sudoku.boxes.filter((e) => e.cellId == 2))}
                 </View>
                 <View style={styles.row}>
-                    {displayCell(sudoku.cells[3])}
-                    {displayCell(sudoku.cells[4])}
-                    {displayCell(sudoku.cells[5])}
+                    {displayCell(sudoku.boxes.filter((e) => e.cellId == 3))}
+                    {displayCell(sudoku.boxes.filter((e) => e.cellId == 4))}
+                    {displayCell(sudoku.boxes.filter((e) => e.cellId == 5))}
                 </View>
                 <View style={styles.row}>
-                    {displayCell(sudoku.cells[6])}
-                    {displayCell(sudoku.cells[7])}
-                    {displayCell(sudoku.cells[8])}
+                    {displayCell(sudoku.boxes.filter((e) => e.cellId == 6))}
+                    {displayCell(sudoku.boxes.filter((e) => e.cellId == 7))}
+                    {displayCell(sudoku.boxes.filter((e) => e.cellId == 8))}
                 </View>
             </View>
         );
@@ -185,35 +188,35 @@ export default function Sudoku({ navigation, route}) {
         return(
             <View style={styles.cell}>
                 <View style={styles.row}>
-                    <Pressable onPress={() => changeFocus(cell[0].cellId, cell[0].id)}>
+                    <Pressable onPress={() => changeFocus(cell[0].id)}>
                         {displayBox(cell[0])}
                     </Pressable>
-                    <Pressable onPress={() => changeFocus(cell[1].cellId, cell[1].id)}>
+                    <Pressable onPress={() => changeFocus(cell[1].id)}>
                         {displayBox(cell[1])}
                     </Pressable>
-                    <Pressable onPress={() => changeFocus(cell[2].cellId, cell[2].id)}>
+                    <Pressable onPress={() => changeFocus(cell[2].id)}>
                         {displayBox(cell[2])}
                     </Pressable>
                 </View>
                 <View style={styles.row}>
-                    <Pressable onPress={() => changeFocus(cell[3].cellId, cell[3].id)}>
+                    <Pressable onPress={() => changeFocus(cell[3].id)}>
                         {displayBox(cell[3])}
                     </Pressable>
-                    <Pressable onPress={() => changeFocus(cell[4].cellId, cell[4].id)}>
+                    <Pressable onPress={() => changeFocus(cell[4].id)}>
                         {displayBox(cell[4])}
                     </Pressable>
-                    <Pressable onPress={() => changeFocus(cell[5].cellId, cell[5].id)}>
+                    <Pressable onPress={() => changeFocus(cell[5].id)}>
                         {displayBox(cell[5])}
                     </Pressable>
                 </View>
                 <View style={styles.row}>
-                    <Pressable onPress={() => changeFocus(cell[6].cellId, cell[6].id)}>
+                    <Pressable onPress={() => changeFocus(cell[6].id)}>
                         {displayBox(cell[6])}
                     </Pressable>
-                    <Pressable onPress={() => changeFocus(cell[7].cellId, cell[7].id)}>
+                    <Pressable onPress={() => changeFocus(cell[7].id)}>
                         {displayBox(cell[7])}
                     </Pressable>
-                    <Pressable onPress={() => changeFocus(cell[8].cellId, cell[8].id)}>
+                    <Pressable onPress={() => changeFocus(cell[8].id)}>
                         {displayBox(cell[8])}
                     </Pressable>
                 </View>
@@ -224,24 +227,24 @@ export default function Sudoku({ navigation, route}) {
         if(box.current == box.solution){ // Display Solution
             return(
                 <View>
-                    {focus !== null && focus[0] == box.cellId && focus[1] == box.id && <Text style={styles.highlightBox}>{box.current}</Text>}
-                    {(focus== null || focus[0] !== box.cellId || focus[1] !== box.id) && <Text style={styles.box}>{box.current}</Text>}
+                    {focus !== null && focus == box.id && <Text style={styles.highlightBox}>{box.current}</Text>}
+                    {(focus== null || focus !== box.id) && <Text style={styles.box}>{box.current}</Text>}
                 </View>
             );
         }
         if(box.current !== '-'){
             return(
                 <View>
-                    {focus !== null && focus[0] == box.cellId && focus[1] == box.id && <Text style={styles.highlightBox}>{box.current}</Text>}
-                    {(focus == null || focus[0] !== box.cellId || focus[1] !== box.id) && <Text style={styles.box}>{box.current}</Text>}
+                    {focus !== null && focus == box.id && <Text style={styles.highlightBox}>{box.current}</Text>}
+                    {(focus == null || focus !== box.id) && <Text style={styles.box}>{box.current}</Text>}
                 </View>
             );
         }
         else{ // Display Temp
             return(
                 <View>
-                    {focus !== null && focus[0] == box.cellId && focus[1] == box.id && <Text style={styles.highlightBox}>{displayTemp(box)}</Text>}
-                    {(focus== null || focus[0] !== box.cellId || focus[1] !== box.id) && <Text style={styles.box}>{displayTemp(box)}</Text>}
+                    {focus !== null && focus == box.id && <Text style={styles.highlightBox}>{displayTemp(box)}</Text>}
+                    {(focus== null || focus !== box.id) && <Text style={styles.box}>{displayTemp(box)}</Text>}
                 </View>
             );
         }
